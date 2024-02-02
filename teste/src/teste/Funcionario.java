3b46e4badeab8ba5be6631a684031536469a7907
Lucas <limalucas7027@gmail.com>
2024-02-02 17:56:07 -0300

    Testando heranca e polimorfismo

---
+++ teste/src/teste/Funcionario.java
@@ -0,0 +1,22 @@
+package teste;
+
+public class Funcionario extends Cliente {
+	int matricula;
+	double salario;
+	
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+	
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	
+	public int getMatricula() {
+		return matricula;
+	}
+	
+	public double getSalario() {
+		return salario;
+	}
+}
---
+++ teste/src/teste/Main.java
@@ -0,0 +1,22 @@
+package teste;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Cliente lucas = new Cliente();
+		lucas.setNome("Lucas de O. Lima");
+		lucas.setIdade(24);
+		
+		System.out.println(lucas.getNome()+" tem " + lucas.getIdade());
+		
+		Funcionario ana = new Funcionario();
+		ana.setNome("Ana Silva");
+		ana.setIdade(30);
+		ana.setMatricula(125);
+		ana.setSalario(1500);
+		
+		System.out.println(ana.getNome()+" tem " + ana.getIdade() + " matricula n# " + ana.getMatricula() + " e recebe " + ana.getSalario());
+
+	}
+
+}
